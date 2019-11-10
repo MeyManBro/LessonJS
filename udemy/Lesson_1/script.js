ebb448aff1978fbfe611b54b9e4896f96aa371ac
MeyMan <alexxlefa@gmail.com>
2019-11-10 10:33:33 +0300

    First commit

---
+++ udemy/Lesson_1/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let answer = true;
+
+let person = {
+    name: "Bob",
+    age: 27,
+    isMarried: answer
+};
+
+console.log("Женат " + person.isMarried);
+
+// let answerUser = prompt ("Есть ли вам 18?", "yes");
+
+// console.log(answerUser);
