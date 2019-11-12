bb975a08b28164045844e55b369c8bf128f4eb39
Aleksey <alexxlefa@gmail.com>
2019-11-12 07:56:30 +0300

    js redact

--- udemy/Lesson_1/script.js
+++ udemy/Lesson_1/script.js
@@ -1,15 +1,32 @@
 'use strict';
 
-let answer = true;
+let budgetMonth = +prompt("Какой ваш бюджет на месяц?" ,''),
+    data =  prompt("введите дату в форамате YYYY-MM-DD", '');   
+     
 
-let person = {
-    name: "Bob",
-    age: 27,
-    isMarried: answer
+let appData = {
+    budget: budgetMonth,
+    timeData: data,
+    expenses: {},
+    optionalExpenses:{},
+    income: [],
+    savings: false
 };
 
-console.log("Женат " + person.isMarried);
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),   
+        b = +prompt("Во сколько обойдется", '');
 
-// let answerUser = prompt ("Есть ли вам 18?", "yes");
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && (typeof(b)) != null && b != '' && a.length < 50) {
+        console.log("save");
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
+};
 
-console.log(answerUser);
+
+appData.budgetPerDay = appData.budget/30;
+alert("Ваш бюджет на 1 день "+ appData.budgetPerDay + " руб.");
+
+console.log(appData);
