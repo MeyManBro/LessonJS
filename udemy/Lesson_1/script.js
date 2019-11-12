ddcc291fd46a605b86951f38d2b6d04f8a54e0ad
Aleksey <alexxlefa@gmail.com>
2019-11-12 14:54:16 +0300

    DZ_2

--- udemy/Lesson_1/script.js
+++ udemy/Lesson_1/script.js
@@ -23,10 +23,58 @@ for (let i = 0; i < 2; i++) {
     } else {
         i--;
     }
-};
+}
+
+// цикл while
+
+// let i = 0;
+// while (i < 2) {
+
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),   
+//         b = +prompt("Во сколько обойдется", '');
+
+//     if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && (typeof(b)) != null && b != '' && a.length < 50) {
+
+//         console.log ("done");
+//         appData.expenses[a] = b;
+//     } else {
+//          i--;
+//     }
+
+//     i++;
+// }
+
+
+// цикл DO...WHILE
+
+// let i = 0;
+// do {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ''),
+//         b = +prompt ("Во сколько обойдется?", '');
+
+//     if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && (typeof(b)) != null && b != '' && a.length < 50) {
+
+//         console.log ("done");
+//         appData.expenses[a] = b;
+//     } else {
+//          i--;
+//     }
+//     i++;
+// }
+// while(i < 2);
 
 
 appData.budgetPerDay = appData.budget/30;
+console.log(appData);
+
 alert("Ваш бюджет на 1 день "+ appData.budgetPerDay + " руб.");
 
-console.log(appData);
+if (appData.budgetPerDay < 100) {
+    alert("Бюджет меньше 100 руб.");
+} else if ((500 < appData.budgetPerDay) && (appData.budgetPerDay < 1000)) {
+    alert("Бюджет средний больше 500руб.");
+} else if (appData.budgetPerDay > 1000) {
+    alert("Да вы богач!");
+}
+
+
