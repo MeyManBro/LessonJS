f42916698c3c7b34bd41cb79eb41cefddfa59ece
Aleksey <alexxlefa@gmail.com>
2019-12-02 16:49:11 +0300

    Apple Site

--- udemy/dz5/js/script.js
+++ udemy/dz5/js/script.js
@@ -1,6 +1,9 @@
 let menuItem = document.querySelectorAll('.menu-item'),
-    menu = document.querySelector('.menu'),
-    bodyMain = document.getElementsByTagName('body');
+    menu = document.querySelector('.menu'),   
+    adv = document.querySelector('.adv'),
+    title = document.getElementById('title'),
+    askQuestion = '',
+    blockPrompt = document.getElementById('prompt');
 
 // document.append.
 console.log(menuItem);
@@ -10,4 +13,19 @@ menu.insertBefore(menuItem[2], menuItem[1]);
 
 // Замена фона у body 
 
-document.body.style.background = 'url(../img/apple_true.jpg) center no-repeat';
+document.body.style.background = 'url(img/apple_true.jpg) center no-repeat';
+
+//Удалить рекламу со страницы
+
+adv.remove();
+
+//добавить слово в заголовок
+
+console.log(title); 
+
+
+// ..задаем вопрос 
+
+askQuestion = prompt(' Как вы относитесь к технике Aplle');
+
+blockPrompt.append('askQuestion');
