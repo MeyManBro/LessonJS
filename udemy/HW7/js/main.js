39c7eecf7128a4df7459d0222e8ec9296f47df11
Aleksey <alexxlefa@gmail.com>
2019-12-04 16:52:04 +0300

    addListenet  end event

--- udemy/HW7/js/main.js
+++ udemy/HW7/js/main.js
@@ -1,21 +1,31 @@
+let startBtn = document.getElementById('start'), //выбрать по id start
+    // let startCalc = document.querySelector('#start');
+    valClass = document.querySelectorAll('div[class*= "value"]'), //выбрать все div где есть value
+    budgetValue = valClass[0],
+    dayBudgetValue = valClass[1],
+    lavelValue = valClass[2],
+    expensesValue = valClass[3],
+    optionalExpensesValue = valClass[4],
+    incomeValue = valClass[5],
+    monthSavingsValue = valClass[6],
+    yearSavingsValue = valClass[7],
 
-let startBtn = document.getElementById('start'),                       //выбрать по id start
-// let startCalc = document.querySelector('#start');
-    valClass = document.querySelectorAll('div[class*= "value"]'),       //выбрать все div где есть value
-    expensesItem = document.getElementsByClassName('expenses-item'),    // input с обязательным расходом.
-    expensesBtn = document.getElementsByTagName('button')[0],           //кнопка утвердить расходы
-    optionalBtn = document.getElementsByTagName('button')[1],           // кнопка утвердить допольнительные расходы
-    countBtn = document.getElementsByTagName('button')[2],              // кнопка расчитать дневной бюджет
-    optExpItem = document.querySelectorAll('.optionalexpenses-item'),   // выбор полей всех допольнительных расходов
-    incomeStat = document.querySelector('#income'),                     // input ввода доходов
-    checkSavings = document.querySelector('#savings'),                  // чек бокс выбора накоплений
-    sumSavings = document.querySelector('#sum'),                        // input сумма накоплений
-    procentSavings = document.querySelector('#percent'),                // input процент годовых накоплений
-    year = document.querySelector('.year-value'),                       // input год
-    month = document.querySelector('.month-value'),                     // input месяц
-    day = document.querySelector('.day-value');                         // input день
+    expensesItem = document.getElementsByClassName('expenses-item'), // input с обязательным расходом.
+    expensesBtn = document.getElementsByTagName('button')[0], //кнопка утвердить расходы
+    optionalBtn = document.getElementsByTagName('button')[1], // кнопка утвердить допольнительные расходы
+    countBtn = document.getElementsByTagName('button')[2], // кнопка расчитать дневной бюджет
+    optExpItem = document.querySelectorAll('.optionalexpenses-item'), // выбор полей всех допольнительных расходов
+    incomeStat = document.querySelector('#income'), // input ввода доходов
+    checkSavings = document.querySelector('#savings'), // чек бокс выбора накоплений
+    sumSavings = document.querySelector('#sum'), // input сумма накоплений
+    precentSavings = document.querySelector('#percent'), // input процент годовых накоплений
+    year = document.querySelector('.year-value'), // input год
+    month = document.querySelector('.month-value'), // input месяц
+    day = document.querySelector('.day-value'); // input день
 
 
-
-
-console.log(procentSavings);
+expensesBtn.addEventListener('click', function(event) {
+    let target = event.target;
+    target.style.display = 'none';
+    // console.log('событие ' + event.type + ' на элементе ' + event.target );
+});
